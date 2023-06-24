3fa72a873c9917298976a468d3d16b98207dd596
Maximilian Mücke <mmuecke@Maximilians-MBP.fritz.box>
2023-06-24 18:57:23 +0200

    feat: added  to readme and added profiling

---
+++ inst/bench/profile.R
@@ -0,0 +1,14 @@
+library(profvis)
+library(mlr3fda)
+library(mlr3pipelines)
+
+profvis({
+  task <- tsk("fuel")
+  po_fmean <- po("ffs", feature = "mean", id = "mean", drop = FALSE)
+  po_fvar <- po("ffs", feature = "var", id = "var", drop = FALSE)
+  po_slope <- po("ffs", feature = "slope", id = "slope", drop = FALSE)
+  graph <- po_fmean %>>%
+    po_fvar %>>%
+    po_slope
+  graph$train(task)
+})
